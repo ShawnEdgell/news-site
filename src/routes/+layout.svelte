7f38fd60b31fd212ce1a6fdfb5b3067735f89e2b
ShawnEdgell <milky.irl@gmail.com>
2024-09-10 12:50:45 -0400

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import '../app.postcss';
+
+	// Skeleton Components
+	import { AppBar } from '@skeletonlabs/skeleton';
+
+	// Highlight JS
+	import hljs from 'highlight.js/lib/core';
+	import 'highlight.js/styles/github-dark.css';
+	import { storeHighlightJs } from '@skeletonlabs/skeleton';
+	import xml from 'highlight.js/lib/languages/xml'; // for HTML
+	import css from 'highlight.js/lib/languages/css';
+	import javascript from 'highlight.js/lib/languages/javascript';
+	import typescript from 'highlight.js/lib/languages/typescript';
+
+	hljs.registerLanguage('xml', xml); // for HTML
+	hljs.registerLanguage('css', css);
+	hljs.registerLanguage('javascript', javascript);
+	hljs.registerLanguage('typescript', typescript);
+	storeHighlightJs.set(hljs);
+
+	// Floating UI for Popups
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+</script>
+
+<div>
+	<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+		<svelte:fragment slot="lead">(icon)</svelte:fragment>
+		(title)
+		<svelte:fragment slot="trail">(actions)</svelte:fragment>
+	</AppBar>
+
+	<slot />
+</div>
